2e7f6d29b21aaec31e224d1488a036b5136351af
Steve Calla <callasteven@gmail.com>
2023-01-11 00:25:10 -0700

    Add client form

--- client/src/components/AdminPortal/AdminMock.js
+++ client/src/components/AdminPortal/AdminMock.js
@@ -1,29 +1,195 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Auth from "../../utils/auth";
 
 import { useQuery } from "@apollo/client";
-import { QUERY_ALL_EMPLOYEES, QUERY_ALL_CLIENTS, QUERY_SCHEDULE } from "../../utils/queries";
+import {
+  QUERY_ALL_EMPLOYEES,
+  QUERY_ALL_CLIENTS,
+  QUERY_SCHEDULE,
+} from "../../utils/queries";
 
-import { Row, Container } from "react-bootstrap";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 import Collapse from "react-bootstrap/Collapse";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../styles/Contact.css";
 
 function AdminMock() {
   const [open, setOpen] = useState(false);
   const [openDetails, setOpenDetails] = useState(false);
-  const [openEmployee, setOpenEmployee] = useState(false);
+  // const [openEmployee, setOpenEmployee] = useState(false);
   // const [open2, setOpen2] = useState(false);
 
   // eslint-disable-next-line
-  const { loading: empLoad, data: emp, error: empError, refetch: empRefectch} = useQuery(QUERY_ALL_EMPLOYEES);
+  const {
+    loading: empLoad,
+    data: emp,
+    error: empError,
+    refetch: empRefectch,
+  } = useQuery(QUERY_ALL_EMPLOYEES);
   console.log(emp);
 
+  // SECTION START CLIENT
   // eslint-disable-next-line
-  const { loading: clientLoad, data: clients, error: clientError, refetch: clientRefetch } = useQuery(QUERY_ALL_CLIENTS);
+  const {
+    loading: clientLoad,
+    data: clients,
+    error: clientError,
+    refetch: clientRefetch,
+  } = useQuery(QUERY_ALL_CLIENTS);
   console.log(clients);
 
+  // GET CLIENT FORM DATA
+  // businessName, streetAddress, suite, city, state, zip, contact, phone, email
+  const [businessName, setBusinessName] = useState("");
+  const [contact, setContact] = useState("");
+  const [phone, setPhone] = useState("");
+  const [emailClient, setEmailClient] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [suite, setSuite] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [areAllFieldsFilled, setAreAllFieldsFilled] = useState(true);
+
+  // VALIDATION
+  const [showBusinessNameValidation, setShowBusinessNameValidation] = useState(false);
+  const [showContactValidation, setShowContactValidation] = useState(false);
+  const [showPhoneValidation, setShowPhoneValidation] = useState(false);
+  const [showEmailClientValidation, setShowEmailClientStateValidation] = useState(false);
+  const [showStreetAddressValidation, setShowStreetAddressValidation] = useState(false);
+  // const [showSuiteValidation, setShowSuiteValidation] = useState(false);
+  const [showCityValidation, setShowCityValidation] = useState(false);
+  const [showStateValidation, setShowStateValidation] = useState(false);
+  const [showZipValidation, setShowZipValidation] = useState(false);
+
+  // Getting the value or name of input triggering change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    // businessName, streetAddress, suite, city, state, zip, contact, phone, email
+    name === "businessName"
+      ? setBusinessName(value)
+      : name === "contact"
+      ? setContact(value)
+      : name === "phone"
+      ? setPhone(value)
+      : name === "emailClient"
+      ? setEmailClient(value)
+      : name === "streetAddress"
+      ? setStreetAddress(value)
+      : name === "suite"
+      ? setSuite(value)
+      : name === "city"
+      ? setCity(value)
+      : name === "state"
+      ? setState(value)
+      : setZip(value)
+
+    console.log('email = ', emailClient);
+
+    return name;
+  };
+
+  // If all fields are populated then enable the submit button
+  useEffect(() => {
+    setAreAllFieldsFilled(
+      businessName.trim() !== "" 
+      &&  contact.trim() !== "" 
+      && phone.trim() !== "" 
+      && emailClient.trim() !== ""
+      && streetAddress.trim() !== "" 
+      // && suite.trim() !== "" 
+      && city.trim() !== "" 
+      && state.trim() !== "" 
+      && zip.trim() !== "" 
+    );
+  console.log(areAllFieldsFilled);
   // eslint-disable-next-line
-  const { loading: scheduleLoad, data: schedule, error: scheduleError, refetch: scheduleRefetch } = useQuery(QUERY_SCHEDULE);
+  }, [
+    businessName,
+    contact,
+    phone,
+    emailClient,
+    streetAddress,
+    suite,
+    city,
+    state,
+    zip,
+  ]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello = ', businessName, streetAddress, suite, city, state, zip, contact, phone, emailClient);
+    // resetForm();
+    // try {
+    //   // eslint-disable-next-line
+    //   const { data } = addIncident({
+    //     variables: {
+    //       employeeName: name,
+    //       locationName,
+    //       employeePhone: telNo,
+    //       subject,
+    //       urgent: isUrgent,
+    //       incidentDetails: body,
+    //     },
+    //   });
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  };
+
+
+  // If user clicks off an input field without entering text, then validation message "is required" displays
+  // businessName, contact, phone, email, streetAddress, suite, city, state, zip
+  const handleBlurChange = (e) => {
+    const { name, value } = e.target;
+
+    name === "businessName" && value.trim() === ""
+      ? setShowBusinessNameValidation(true)
+      : setShowBusinessNameValidation(false);
+    name === "contact" && value.trim() === ""
+      ? setShowContactValidation(true)
+      : setShowContactValidation(false);
+    name === "phone" && value.trim() === ""
+      ? setShowPhoneValidation(true)
+      : setShowPhoneValidation(false);
+    name === "emailClient" && value.trim() === ""
+      ? setShowEmailClientStateValidation(true)
+      : setShowEmailClientStateValidation(false);
+    name === "streetAddress" && value.trim() === ""
+      ? setShowStreetAddressValidation(true)
+      : setShowStreetAddressValidation(false);
+    // name === "suite" && value.trim() === ""
+    //   ? setShowSuiteValidation(true)
+    //   : setShowSuiteValidation(false);
+    name === "city" && value.trim() === ""
+      ? setShowCityValidation(true)
+      : setShowCityValidation(false);
+    name === "state" && value.trim() === ""
+      ? setShowStateValidation(true)
+      : setShowStateValidation(false);
+    name === "zip" && value.trim() === ""
+      ? setShowZipValidation(true)
+      : setShowZipValidation(false);
+  };
+
+  // ADD CLIENT
+
+  // UPDATE CLIENT
+
+  // DELETE CLIENT
+
+  // SECTION END CLIENT
+
+  // eslint-disable-next-line
+  const {
+    loading: scheduleLoad,
+    data: schedule,
+    error: scheduleError,
+    refetch: scheduleRefetch,
+  } = useQuery(QUERY_SCHEDULE);
   console.log(schedule);
 
   // collapse show / not show detail
@@ -47,7 +213,9 @@ function AdminMock() {
   // const [showHidePassword, setShowHidePassword] = useState("password");
 
   const handleToggle = (toggle) => {
-    toggle === "admin" ? setAdminToggle(!adminToggle): setLockedToggle(!lockedToggle);
+    toggle === "admin"
+      ? setAdminToggle(!adminToggle)
+      : setLockedToggle(!lockedToggle);
 
     // if (showHidePassword === "password") {
     //   setShowHidePassword("test");
@@ -76,7 +244,11 @@ function AdminMock() {
                 <div
                   className="rounded directions-collapse"
                   id="headingOne"
-                  style={{ color: "black", display: "flex", justifyContent: "space-between" }}
+                  style={{
+                    color: "black",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
                 >
                   <h5 className="mb-0 text-left">
                     <button
@@ -89,8 +261,8 @@ function AdminMock() {
                       {emp?.firstName} {emp?.lastName}
                     </button>
                   </h5>
-                  <div className="mr-2" style={{ display: "flex"}}>
-                  <FontAwesomeIcon
+                  <div className="mr-2" style={{ display: "flex" }}>
+                    <FontAwesomeIcon
                       icon="fa-add"
                       className="p-2"
                       onClick={() => console.log("pencil")}
@@ -158,7 +330,9 @@ function AdminMock() {
                         <div>Start Time: {job?.startTime}</div>
                         <div>End Date: {job?.endDate}</div>
                         <div>Job Details: {job?.jobDetails}</div>
-                        <div>Number of Clients: {job?.numberOfClientEmployees}</div>
+                        <div>
+                          Number of Clients: {job?.numberOfClientEmployees}
+                        </div>
                       </div>
                     ))}
                   </div>
@@ -182,7 +356,7 @@ function AdminMock() {
                 >
                   <h5 className="mb-0 text-left">
                     <button
-                    //section
+                      //section
                       onClick={(event) => getElement(event)}
                       aria-controls={`#collapse-client-${index}`}
                       aria-expanded={openDetails}
@@ -211,7 +385,9 @@ function AdminMock() {
                         <div>Start Time: {job?.startTime}</div>
                         <div>End Date: {job?.endDate}</div>
                         <div>Job Details: {job?.jobDetails}</div>
-                        <div>Number of Clients: {job?.numberOfClientEmployees}</div>
+                        <div>
+                          Number of Clients: {job?.numberOfClientEmployees}
+                        </div>
                       </div>
                     ))}
                   </div>
@@ -221,6 +397,245 @@ function AdminMock() {
           ))}
         </Row>
       </Container>
+
+      {/* section client form */}
+      <div
+        className="mx-3 pb-2 d-flex flex-column align-self-center align-items-center shadow rounded-lg border border-secondary"
+        style={{ margin: "20px 0px 20px 0px", textAlign: "center" }}
+      >
+        {/* <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+          style={{ marginTop: "10px" }}
+        >
+          Add New Client
+        </Button> */}
+
+        <Form
+          className="py-3 overflow-auto custom-about"
+          onSubmit={handleFormSubmit}
+          style={{ width: "80vw" }}
+        >
+          <div className="d-flex justify-content-center">
+            <h2 className="display-5 custom-text heading">Client Form</h2>
+
+            <FontAwesomeIcon
+              icon="fa-add"
+              className="p-2"
+
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+
+              title="Add new client"
+              transform="grow-10"
+              alt="Share my profile"
+
+              // onClick={() => console.log("add")}
+              // onClick={() => handlePassClick()}
+              // style={display ? isDisplayed : isNotDisplayed}
+            />
+          </div>
+          <Collapse in={open}>
+          <div id="example-collapse-text">
+            <Form.Group className="mb-3 form-length" controlId="formBasicEmail">
+              <div className="form-label">
+                <Form.Label style={{ fontWeight: "bolder" }}>
+                  Company Name
+                </Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showBusinessNameValidation ? "show" : "hide"
+                  }`}
+                >
+                  * field is required
+                </Form.Label>
+              </div>
+              <Form.Control
+                className="custom-border"
+                type="text"
+                placeholder="Enter Company Name"
+                name="businessName"
+                defaultValue="test"
+                onChange={handleInputChange}
+                onBlur={handleBlurChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-length" controlId="formBasicEmail">
+              <div className="form-label">
+                <Form.Label style={{ fontWeight: "bolder" }}>
+                  Contact Name
+                </Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showContactValidation ? "show" : "hide"
+                  }`}
+                >
+                  * field is required
+                </Form.Label>
+              </div>
+              <Form.Control
+                className="custom-border"
+                type="text"
+                placeholder="Enter Contact Person"
+                name="contact"
+                defaultValue="test contact"
+                onChange={handleInputChange}
+                onBlur={handleBlurChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-length" controlId="formBasicEmail">
+              <div className="form-label">
+                <Form.Label style={{ fontWeight: "bolder" }}>
+                  Phone Number
+                </Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showPhoneValidation ? "show" : "hide"
+                  }`}
+                >
+                  * field is required
+                </Form.Label>
+              </div>
+              <Form.Control
+                className="custom-border"
+                type="tel"
+                placeholder="example: 123-456-7899"
+                name="phone"
+                defaultValue="123-456-7899"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                onChange={handleInputChange}
+                onBlur={handleBlurChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-length" controlId="formBasicEmail">
+              <div className="form-label">
+                <Form.Label style={{ fontWeight: "bolder" }}>Email</Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showEmailClientValidation ? "show" : "hide"
+                  }`}
+                >
+                  * field is required
+                </Form.Label>
+              </div>
+              <Form.Control
+                className="custom-border"
+                type="email"
+                placeholder="Client Email"
+                name="emailClient"
+                defaultValue="test@test.com"
+                onChange={handleInputChange}
+                onBlur={handleBlurChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-length" controlId="formBasicEmail">
+              <div className="form-label">
+                <Form.Label style={{ fontWeight: "bolder" }}>
+                  Address
+                </Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showStreetAddressValidation ? "show" : "hide"
+                  }`}
+                >
+                  * field is required
+                </Form.Label>
+              </div>
+              <Form.Control
+                className="custom-border"
+                placeholder="Enter Address"
+                name="streetAddress"
+                defaultValue="test address"
+                onChange={handleInputChange}
+                onBlur={handleBlurChange}
+                required
+              />
+            </Form.Group>
+
+            <Row className="addy">
+              <Col xs={6}>
+                <Form.Label style={{ fontWeight: "bolder" }}>City</Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showCityValidation ? "show" : "hide"
+                  }`}
+                >
+                  * required
+                </Form.Label>
+                <Form.Control
+                  className="custom-border"
+                  placeholder="City"
+                  name="city"
+                  defaultValue="test city"
+                  onChange={handleInputChange}
+                  onBlur={handleBlurChange}
+                  required
+                />
+              </Col>
+              <Col>
+                <Form.Label style={{ fontWeight: "bolder" }}>State</Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showStateValidation ? "show" : "hide"
+                  }`}
+                >
+                  * required
+                </Form.Label>
+                <Form.Control
+                  className="custom-border"
+                  placeholder="State"
+                  name="state"
+                  defaultValue="CO"
+                  onChange={handleInputChange}
+                  onBlur={handleBlurChange}
+                  required
+                />
+              </Col>
+              <Col>
+                <Form.Label style={{ fontWeight: "bolder" }}>
+                  Zipcode
+                </Form.Label>
+                <Form.Label
+                  className={`validation-color ${
+                    showZipValidation ? "show" : "hide"
+                  }`}
+                >
+                  * required
+                </Form.Label>
+                <Form.Control
+                  className="custom-border"
+                  placeholder="Zip"
+                  name="zip"
+                  defaultValue="07801"
+                  onChange={handleInputChange}
+                  onBlur={handleBlurChange}
+                  required
+                />
+              </Col>
+            </Row>
+            <Button
+              className="button-custom submit-button-style"
+              variant="primary"
+              type="submit"
+              disabled={!areAllFieldsFilled}
+              title="Enter all fields to add a new client"
+            >
+              Add Client
+            </Button>
+          </div>
+          </Collapse>
+        </Form>
+      </div>
 
       <Container style={{ border: "1px solid black" }}>
         <h3>Schedule</h3>
@@ -235,7 +650,7 @@ function AdminMock() {
                 >
                   <h5 className="mb-0 text-left">
                     <button
-                    // //section
+                      // //section
                       onClick={(event) => getElement(event)}
                       aria-controls={`#collapse-schedule-${index}`}
                       aria-expanded={openDetails}
@@ -278,7 +693,6 @@ function AdminMock() {
           ))}
         </Row>
       </Container>
-
     </>
   );
 }
