46bbba59be229bfedac9874dd6fbe5230349328c
Bryan Wienhoff <bhoff1980@gmail.com>
2023-01-18 19:57:59 -0700

    new image on 404 page

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -24,10 +24,7 @@ function GenericModal({
         Launch demo modal
       </Button> */}
 
-      <Modal 
-        size="md"
-        show={show} 
-        onHide={handleClose}>
+      <Modal size="md" show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{schedule.client.businessName}</Modal.Title>
         </Modal.Header>
@@ -35,26 +32,35 @@ function GenericModal({
           {format_date_MMDDYYYY(schedule.startDate)}
         </Modal.Title>
         <Modal.Body className="py-2">
-        <a href={`https://www.google.com/maps/dir/?api=1&destination=${schedule.streetAddress},${schedule.city},${schedule.state},${schedule.zip}&travelmode=driving`}
-        target="_blank"
-                          rel="noreferrer">
-                          <FontAwesomeIcon
-                            icon="fa-solid fa-location-dot"
-                            style={{ marginTop: "4px", marginRight: "5px" }}
-                          />
-          {schedule.streetAddress}, {schedule.city} {schedule.state}{" "}
-          {schedule.zip}</a>
+          <a
+            href={`https://www.google.com/maps/dir/?api=1&destination=${schedule.streetAddress},${schedule.city},${schedule.state},${schedule.zip}&travelmode=driving`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon="fa-solid fa-location-dot"
+              style={{ marginTop: "4px", marginRight: "5px" }}
+            />
+            {schedule.streetAddress}, {schedule.city} {schedule.state}{" "}
+            {schedule.zip}
+          </a>
         </Modal.Body>
-        <Modal.Body className="py-2">    <a href={`mailto:${schedule.client.email}`}>
-                          {" "}
-                          <FontAwesomeIcon icon="fa-solid fa-envelope-open-text" />{" "}
-                          {schedule.client.email}
-                        </a></Modal.Body>
+        <Modal.Body className="py-2">
+          {" "}
+          <a href={`mailto:${schedule.client.email}`}>
+            {" "}
+            <FontAwesomeIcon icon="fa-solid fa-envelope-open-text" />{" "}
+            {schedule.client.email}
+          </a>
+        </Modal.Body>
 
-        <Modal.Body className="py-2"> <a href={`tel:+${schedule.client.phone}`}>
-                            <FontAwesomeIcon icon="fa-solid fa-phone"></FontAwesomeIcon>{" "}
-                            {schedule.client.phone}
-                          </a></Modal.Body>
+        <Modal.Body className="py-2">
+          {" "}
+          <a href={`tel:+${schedule.client.phone}`}>
+            <FontAwesomeIcon icon="fa-solid fa-phone"></FontAwesomeIcon>{" "}
+            {schedule.client.phone}
+          </a>
+        </Modal.Body>
         <Modal.Body className="py-2">{schedule.jobDetails}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
