32f2aacffd3dd56f11e7763f5774372314bea426
Steve Calla <callasteven@gmail.com>
2023-01-19 01:10:32 -0700

    Add tiny url

--- client/src/components/EmailSend/index.js
+++ client/src/components/EmailSend/index.js
@@ -16,27 +16,29 @@ import { TINY_URL_PATH, getTinyURL } from "../../utils/tinyURL";
 function useEmailSend(props) {
   console.log("useEmail hook,props = ", props);
 
+  // SECTION get tiny url
   const [tinyURI, setTinyURI] = useState("");
 
-  getTinyURL(props.token).then((data) => {
-    setTinyURI(data.data.tiny_url);
-  });
+  // getTinyURL(props.token).then((data) => {
+  //   setTinyURI(data.data.tiny_url);
+  // });
+  // console.log("tokenURL = ", tinyURI);
 
-  console.log('tokenURL = ', tinyURI);
-
+  // SECTION SET EMAIL CONTENT
   // const toEmail = props.toEmail ? "callasteven@gmail.com" : "";
   const toEmail = "callasteven@gmail.com";
   const fromEmail = FROM_EMAIL;
   const subject = props.source === "resetPassword" ? RESET_SUBJECT : "TBD";
   const textContent =
     props.source === "resetPassword"
-      ? reset_text_template(props.tokenURL, props.firstName)
+      ? reset_text_template(tinyURI, props.firstName)
       : "TBD";
   const htmlContent =
     props.source === "resetPassword"
-      ? reset_html_template(props.tokenURL, props.firstName)
+      ? reset_html_template(tinyURI, props.firstName)
       : "TBD";
 
+  // SECTION TO SEND EMAIL VIA LAZY QUERY
   // eslint-disable-next-line
   const [
     sendEmail,
@@ -52,10 +54,20 @@ function useEmailSend(props) {
     fetchPolicy: "cache-and-network", // ensure the query executes after each click
   });
 
+  // SECTION USE EFFECT TO RUN SENDEMAIL IF TOKEN IS POPULATED (since this hook will run on every render for this component)
   useEffect(() => {
-    // console.log('use effect = ', props, props.token )
-    if (props.token) {
+    console.log('use effect = ', props, props.token, !props.token, Object.keys(props).length !== 0  )
+
+    if (props.token || Object.keys(props).length !== 0) {
+      // get tiny url
+      getTinyURL(props.token).then((data) => {
+        setTinyURI(data.data.tiny_url);
+      });
+      console.log("tokenURL = ", tinyURI);
+
       // console.log('props passed');
+
+      // send email
       sendEmail();
 
       if (emailError) {
