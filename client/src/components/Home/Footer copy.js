3e197a3978d8947549d54f618baa91c8b0bcba9d
Steve Calla <callasteven@gmail.com>
2023-01-22 21:37:52 -0700

    Refactor footer

--- client/src/components/Home/Footer copy.js
+++ client/src/components/Home/Footer copy.js
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
 import { Row, Col, Container, Nav,} from "react-bootstrap";
-import "../../styles/footer.css";
+import "../../styles/home.css";
 import logo from "../../assets/images/logo.bkg.png";
 
 function Footer () {
     return (       
     <footer >
-        <div className="m-3">
+        <Container>
+        {/* <h3 className="footer-title">
+        Office Furniture Installation At The Snap Of Your Fingers!
+        </h3> */}
         <h3 className="footer-title">
           Serving Greater Denver for Over 15 Years
         </h3> 
-          <Row className="logo-phone-container">
-            <Col className="logo-container">
-              <img className='full-logo' src={logo}  alt="logo"></img>
+          <Row>
+            <Col sm={5} md={4} xs={6}>
+              <img style={{maxWidth:'200px', height: "25vh"}}src={logo} className='full-logo' alt="logo"></img>
             </Col>
-            <Col className="m-0 footer-phone">
+            <Col sm={6} md={4} xs={6} className="footer-phone">
               <p className="number" style= {{marginTop:'30px'}}>(555)-555-5555</p>
               <p className="espanol">Se Habla Español!</p>
-            </Col>
 
-            <Col className="d-flex justify-content-center p-0 footer-nav">
+            </Col>
+            <Col className="d-flex footer-nav">
               <Nav.Link className="footer-nav" as={Link} to="/shoplinks">
                 Shop Furniture
               </Nav.Link>
@@ -32,7 +35,7 @@ function Footer () {
             </Col>
           </Row>
           <p className="footer-copy">Copyright &copy;{new Date().getFullYear()} Integral Solutions LLC All Rights Reserved.</p>
-        </div>
+        </Container>
       </footer>)
 
 }
