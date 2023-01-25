6a8f854f07fe56312e4c44579d2dbf2be0be7a88
Steve Calla <callasteven@gmail.com>
2023-01-25 04:51:06 -0700

    Added created at and updated dates to all models

--- server/models/Client.js
+++ server/models/Client.js
@@ -1,49 +1,54 @@
 const { Schema, model } = require("mongoose");
 
-const clientSchema = new Schema({
-  businessName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const clientSchema = new Schema(
+  {
+    businessName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    streetAddress: {
+      type: String,
+      required: true,
+    },
+    suite: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    schedule: {
+      type: Schema.Types.Array,
+      ref: "Schedule",
+    },
   },
-  streetAddress: {
-    type: String,
-    required: true,
-  },
-  suite: {
-    type: String,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  zip: {
-    type: String,
-    required: true,
-  },
-  contact: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    match: [/.+@.+\..+/, "Must use a valid email address"],
-  },
-  schedule: {
-    type: Schema.Types.Array,
-    ref: "Schedule",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Client = model("Client", clientSchema);
 
