f28801fa63bae59505d0b5b4f3026922c60b222b
RodBennett <rod.bennett75@gmail.com>
2023-01-26 18:09:08 -0700

    merge pull

--- client/src/components/Home/Footer.js
+++ client/src/components/Home/Footer.js
@@ -13,34 +13,28 @@ function Footer() {
         <main className="footer-container">
           <img className="full-logo" src={logo} alt="logo"></img>
           <section className="phone-container">
-            <p className="">(555)-555-5555</p>
+            <p className="">(720)-232-2510</p>
             <p className="espanol">Se Habla Español!</p>
           </section>
-        </h3> 
-          <Row>
-            <Col sm={5} md={4} xs={6}>
-              <img style={{maxWidth:'200px', height: "25vh"}}src={logo} className='full-logo' alt="logo"></img>
-            </Col>
-            <Col sm={6} md={4} xs={6} className="footer-phone">
-              <p className="number" style= {{marginTop:'30px'}}>(720)-232-2510</p>
-              <p className="espanol">Se Habla Español!</p>
 
-            </Col>
-            <Col className="d-flex footer-nav">
-              <Nav.Link className="footer-nav" as={Link} to="/shoplinks">
-                Shop Furniture
-              </Nav.Link>
-              <Nav.Link className="footer-nav" as={Link} to="/contact">
-                Contact Us{" "}
-              </Nav.Link>
-              <Nav.Link className="footer-nav" as={Link} to="/login">
-                Employee Login 
-              </Nav.Link>
-            </Col>
-          </Row>
-          <p className="footer-copy">Copyright &copy;{new Date().getFullYear()} Integral Solutions LLC All Rights Reserved.</p>
-        </Container>
-      </footer>)
+          <section className="footer-nav-menu">
+            <Nav.Link className="menu-text" as={Link} to="/shoplinks">
+              Shop Furniture
+            </Nav.Link>
+            <Nav.Link className="menu-text" as={Link} to="/contact">
+              Contact Us{" "}
+            </Nav.Link>
+            <Nav.Link className="menu-text" as={Link} to="/login">
+              Employee Login
+            </Nav.Link>
+          </section>
+        </main>
 
+        <p className="footer-copy">
+          &copy;{new Date().getFullYear()} Integral Solutions LLC.
+        </p>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
